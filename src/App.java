fbd01e5f1599f7ff2daf47049bf7fd8b30421117
Gonca Çomak <gonca.0045@hotmail.com>
2024-06-26 17:44:53 +0300

    Tourism Agency System Update

--- src/App.java
+++ src/App.java
@@ -2,12 +2,22 @@ import business.UserManager;
 import core.Helper;
 import view.LoginView;
 
+
 public class App {
     public static void main(String[] args) {
         // proje başlatma !
+        // Admin : gonci
+        // pass : 0000
+
+        // Employee : kenan
+        // pass : 0000
+
+
 
         Helper.setTheme();
-        //UserManager userManager = new UserManager();
+        UserManager userManager = new UserManager();
         LoginView loginView = new LoginView();
+
+
     }
 }
